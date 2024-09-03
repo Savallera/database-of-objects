ea907b84ea13a76b3cdab539c6a2208fae33dbd9
Savallera <phenix-vn@yandex.ru>
2024-09-03 11:55:04 +0300

    header adaptive

---
+++ src/blocks/header/header.js
@@ -0,0 +1,19 @@
+const mobileMenuButton = document.querySelector('.header__mobile-menu-button');
+const header = document.querySelector('.header');
+
+const openMobileMenu = function () {
+  header.classList.add('header--opened');
+};
+const closeMobileMenu = function () {
+  header.classList.remove('header--opened');
+};
+
+mobileMenuButton.addEventListener('click', openMobileMenu);
+header.addEventListener('click', (evt) => {
+  if (
+    evt.target.classList.contains('header--opened') ||
+    evt.target.classList.contains('header__mobile-menu-button')
+  ) {
+    closeMobileMenu();
+  }
+});
