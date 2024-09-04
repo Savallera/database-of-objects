bed9b9f5bb2999d4830e528890a32a03fa1658fe
Savallera <phenix-vn@yandex.ru>
2024-09-04 14:20:13 +0300

    main-info page

--- src/blocks/gallery-slider/gallery-slider.js
+++ src/blocks/gallery-slider/gallery-slider.js
@@ -3,6 +3,12 @@ window.addEventListener('DOMContentLoaded', () => {
     // Слайдер новостей на главной странице
     if (document.querySelector('#gallery-slider')) {
       let gallerySlider = new Splide('#gallery-slider', {
+        classes: {
+          arrows: 'splide__arrows gallery-slider__arrows',
+          arrow: 'splide__arrow gallery-slider__arrow',
+          prev: 'splide__arrow--prev gallery-slider__arrow--prev',
+          next: 'splide__arrow--next gallery-slider__arrow--next',
+        },
         type: 'slide',
         pagination: false,
         arrows: true,
