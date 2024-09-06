bfb83b4c3cf6f14e26f2023fb65f1e33f183c7e3
Savallera <phenix-vn@yandex.ru>
2024-09-06 13:56:52 +0300

    service page

---
+++ src/blocks/accordion/accordion.js
@@ -0,0 +1,24 @@
+function toggleAccordion(target) {
+  let container = target.parentElement;
+  let section = container.lastElementChild;
+  let transitionDuration =
+    parseFloat(getComputedStyle(section)['transitionDuration']) * 1000;
+
+  if (container.dataset.slow) {
+    if (container.classList.contains('accordion--opened')) {
+      section.style.maxHeight = section.scrollHeight + 'px';
+      section.style.overflow = null;
+      setTimeout(() => {
+        section.style.maxHeight = null;
+      }, transitionDuration);
+    } else {
+      section.style.maxHeight = section.scrollHeight + 'px';
+      setTimeout(() => {
+        section.style.overflow = 'unset';
+        section.style.maxHeight = 'unset';
+      }, transitionDuration);
+    }
+  }
+
+  container.classList.toggle('accordion--opened');
+}
--- src/blocks/gallery-slider/gallery-slider.js
+++ src/blocks/gallery-slider/gallery-slider.js
@@ -16,5 +16,51 @@ window.addEventListener('DOMContentLoaded', () => {
       });
       gallerySlider.mount();
     }
+    // Для галереи
+    if (document.querySelector('#gallery-slider-02')) {
+      let gallerySlider = new Splide('#gallery-slider-02', {
+        classes: {
+          arrows: 'splide__arrows gallery-slider__arrows',
+          arrow: 'splide__arrow gallery-slider__arrow',
+          prev: 'splide__arrow--prev gallery-slider__arrow--prev',
+          next: 'splide__arrow--next gallery-slider__arrow--next',
+        },
+        type: 'slide',
+        pagination: false,
+        arrows: true,
+        gap: '1em',
+      });
+      gallerySlider.mount();
+    }
+    if (document.querySelector('#gallery-slider-03')) {
+      let gallerySlider = new Splide('#gallery-slider-03', {
+        classes: {
+          arrows: 'splide__arrows gallery-slider__arrows',
+          arrow: 'splide__arrow gallery-slider__arrow',
+          prev: 'splide__arrow--prev gallery-slider__arrow--prev',
+          next: 'splide__arrow--next gallery-slider__arrow--next',
+        },
+        type: 'slide',
+        pagination: false,
+        arrows: true,
+        gap: '1em',
+      });
+      gallerySlider.mount();
+    }
+    if (document.querySelector('#gallery-slider-04')) {
+      let gallerySlider = new Splide('#gallery-slider-04', {
+        classes: {
+          arrows: 'splide__arrows gallery-slider__arrows',
+          arrow: 'splide__arrow gallery-slider__arrow',
+          prev: 'splide__arrow--prev gallery-slider__arrow--prev',
+          next: 'splide__arrow--next gallery-slider__arrow--next',
+        },
+        type: 'slide',
+        pagination: false,
+        arrows: true,
+        gap: '1em',
+      });
+      gallerySlider.mount();
+    }
   }
 });
