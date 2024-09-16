020996b3c7c336e7374d72bdf1b0aa72a30b1b5d
Savallera <phenix-vn@yandex.ru>
2024-09-16 15:22:14 +0300

    search, monitoring pages

--- src/blocks/accordion/accordion.js
+++ src/blocks/accordion/accordion.js
@@ -1,24 +1,6 @@
 function toggleAccordion(target) {
   let container = target.closest('.accordion');
-  let section = container.lastElementChild;
-  let transitionDuration =
-    parseFloat(getComputedStyle(section)['transitionDuration']) * 1000;
 
-  if (container.dataset.slow) {
-    if (container.classList.contains('accordion--opened')) {
-      section.style.maxHeight = section.scrollHeight + 'px';
-      section.style.overflow = null;
-      setTimeout(() => {
-        section.style.maxHeight = null;
-      }, transitionDuration);
-    } else {
-      section.style.maxHeight = section.scrollHeight + 'px';
-      setTimeout(() => {
-        section.style.overflow = 'unset';
-        section.style.maxHeight = 'unset';
-      }, transitionDuration);
-    }
-  }
-
+  target.classList.toggle('accordion__button--actived');
   container.classList.toggle('accordion--opened');
 }
