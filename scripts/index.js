27ab28386a42f7eb346502190731a3b92dda2027
Savallera <phenix-vn@yandex.ru>
2024-10-30 15:10:57 +0300

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -154,6 +154,50 @@ if (document.querySelector('.search__button--content-objects')) {
     }
   });
 }
+if (document.querySelector('.search__button--content-zones')) {
+  const objectsButton = document.querySelector(
+    '.search__button--content-zones'
+  );
+  const objectsModal = document.querySelector('.modal--content-zones');
+  const openObjectsModal = function () {
+    objectsModal.classList.add('modal--opened');
+  };
+  const closeObjectsModal = function () {
+    objectsModal.classList.remove('modal--opened');
+  };
+
+  objectsButton.addEventListener('click', openObjectsModal);
+  objectsModal.addEventListener('click', (evt) => {
+    if (
+      evt.target.classList.contains('modal--content-zones') ||
+      evt.target.classList.contains('modal__close-button')
+    ) {
+      closeObjectsModal();
+    }
+  });
+}
+if (document.querySelector('.search__button--content-services')) {
+  const objectsButton = document.querySelector(
+    '.search__button--content-services'
+  );
+  const objectsModal = document.querySelector('.modal--content-services');
+  const openObjectsModal = function () {
+    objectsModal.classList.add('modal--opened');
+  };
+  const closeObjectsModal = function () {
+    objectsModal.classList.remove('modal--opened');
+  };
+
+  objectsButton.addEventListener('click', openObjectsModal);
+  objectsModal.addEventListener('click', (evt) => {
+    if (
+      evt.target.classList.contains('modal--content-services') ||
+      evt.target.classList.contains('modal__close-button')
+    ) {
+      closeObjectsModal();
+    }
+  });
+}
 
 /* Прелодер на страницы */
 const preloaderTmp = document.querySelector('.preloader-template');
